5ecaee9bada5fa76ecf9b4c77d61dae109289ac3
MajorTomAW <tom.wrnr.private@gmail.com>
2026-02-23 14:24:39 +0100

    added AkGameplayCueNotify_Looping class

---
+++ Source/WwiseGameplayCues/Private/AkGameplayCueNotify_Looping.cpp
@@ -0,0 +1,161 @@
+﻿// Author: Tom Werner (MajorT), 2026 February
+
+
+#include "AkGameplayCueNotify_Looping.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AkGameplayCueNotify_Looping)
+
+AAkGameplayCueNotify_Looping::AAkGameplayCueNotify_Looping(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	bAutoDestroyOnRemove = true;
+	bAllowMultipleWhileActiveEvents = false;
+	NumPreallocatedInstances = 3;
+
+	DefaultPlacementInfo.AttachPolicy = EGameplayCueNotify_AttachPolicy::AttachToTarget;
+
+	Recycle();
+}
+
+bool AAkGameplayCueNotify_Looping::Recycle()
+{
+	Super::Recycle();
+
+	// Extra check to make sure looping effects have been removed.  Normally they will have been removed in the OnRemove event.
+	RemoveLoopingEffects();
+
+	ApplicationSpawnResults.Reset();
+	LoopingSpawnResults.Reset();
+	RecurringSpawnResults.Reset();
+	RemovalSpawnResults.Reset();
+
+	bLoopingEffectsRemoved = true;
+
+	return true;
+}
+
+bool AAkGameplayCueNotify_Looping::OnActive_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters)
+{
+	UWorld* World = GetWorld();
+
+	FGameplayCueNotify_SpawnContext SpawnContext(World, MyTarget, Parameters);
+	SpawnContext.SetDefaultSpawnCondition(&DefaultSpawnCondition);
+	SpawnContext.SetDefaultPlacementInfo(&DefaultPlacementInfo);
+
+	if (DefaultSpawnCondition.ShouldSpawn(SpawnContext))
+	{
+		ApplicationEffects.ExecuteEffects(SpawnContext, ApplicationSpawnResults);
+
+		OnApplication(MyTarget, Parameters, ApplicationSpawnResults);
+	}
+
+	return false;
+}
+
+bool AAkGameplayCueNotify_Looping::WhileActive_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters)
+{
+	UWorld* World = GetWorld();
+
+	FGameplayCueNotify_SpawnContext SpawnContext(World, MyTarget, Parameters);
+	SpawnContext.SetDefaultSpawnCondition(&DefaultSpawnCondition);
+	SpawnContext.SetDefaultPlacementInfo(&DefaultPlacementInfo);
+
+	if (DefaultSpawnCondition.ShouldSpawn(SpawnContext))
+	{
+		bLoopingEffectsRemoved = false;
+		LoopingEffects.StartEffects(SpawnContext, LoopingSpawnResults);
+
+		OnLoopingStart(MyTarget, Parameters, LoopingSpawnResults);
+	}
+
+	return false;
+}
+
+bool AAkGameplayCueNotify_Looping::OnExecute_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters)
+{
+	UWorld* World = GetWorld();
+
+	FGameplayCueNotify_SpawnContext SpawnContext(World, MyTarget, Parameters);
+	SpawnContext.SetDefaultSpawnCondition(&DefaultSpawnCondition);
+	SpawnContext.SetDefaultPlacementInfo(&DefaultPlacementInfo);
+
+	if (DefaultSpawnCondition.ShouldSpawn(SpawnContext))
+	{
+		RecurringEffects.ExecuteEffects(SpawnContext, RecurringSpawnResults);
+
+		OnRecurring(MyTarget, Parameters, RecurringSpawnResults);
+	}
+
+	return false;
+}
+
+bool AAkGameplayCueNotify_Looping::OnRemove_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters)
+{
+	RemoveLoopingEffects();
+
+	// Don't spawn removal effects if our target is gone
+	if (IsValid(MyTarget))
+	{
+		UWorld* World = GetWorld();
+
+		FGameplayCueNotify_SpawnContext SpawnContext(World, MyTarget, Parameters);
+		SpawnContext.SetDefaultSpawnCondition(&DefaultSpawnCondition);
+		SpawnContext.SetDefaultPlacementInfo(&DefaultPlacementInfo);
+
+		if (DefaultSpawnCondition.ShouldSpawn(SpawnContext))
+		{
+			RemovalEffects.ExecuteEffects(SpawnContext, RemovalSpawnResults);
+		}
+	}
+
+	// Always call OnRemoval(), even if target is bad, so it can clean up BP-spawned things.
+	OnRemoval(MyTarget, Parameters, RemovalSpawnResults);
+
+	return false;
+}
+
+void AAkGameplayCueNotify_Looping::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (EndPlayReason == EEndPlayReason::Destroyed)
+	{
+		RemoveLoopingEffects();
+	}
+
+	Super::EndPlay(EndPlayReason);
+}
+
+#if WITH_EDITOR
+EDataValidationResult AAkGameplayCueNotify_Looping::IsDataValid(class FDataValidationContext& Context) const
+{
+	TArray<FText> ValidationErrors;
+	ApplicationEffects.ValidateAssociatedAssets(this, TEXT("ApplicationEffects"), Context);
+	LoopingEffects.ValidateAssociatedAssets(this, TEXT("LoopingEffects"), Context);
+	RecurringEffects.ValidateAssociatedAssets(this, TEXT("RecurringEffects"), Context);
+	RemovalEffects.ValidateAssociatedAssets(this, TEXT("RemovalEffects"), Context);
+
+	return ((ValidationErrors.Num() > 0) ? EDataValidationResult::Invalid : EDataValidationResult::Valid);
+}
+#endif
+
+void AAkGameplayCueNotify_Looping::RemoveLoopingEffects()
+{
+	if (bLoopingEffectsRemoved)
+	{
+		return;
+	}
+
+	bLoopingEffectsRemoved = true;
+	LoopingEffects.StopEffects(LoopingSpawnResults);
+}
---
+++ Source/WwiseGameplayCues/Public/AkGameplayCueNotify_Looping.h
@@ -0,0 +1,105 @@
+﻿/** Author: Tom Werner (MajorT), 2026 February */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AkGameplayCueTypes.h"
+#include "GameplayCueNotifyTypes.h"
+#include "GameplayCueNotify_Actor.h"
+
+#include "AkGameplayCueNotify_Looping.generated.h"
+
+#define UE_API WWISEGAMEPLAYCUES_API
+
+struct FAkGameplayCueNotify_SpawnResult;
+/**
+ * AAkGameplayCueNotify_Looping
+ *
+ *	This is an instanced gameplay cue notify for continuous looping effects.
+ *	The game is responsible for defining the start/stop by adding/removing the gameplay cue.
+ *
+ *	Supporting Ak (Wwise) audio events.
+ */
+UCLASS(Blueprintable, NotPlaceable, Category="GameplayCueNotify", MinimalAPI, meta=(ShowWorldContextPin, DisplayName="Ak GCN Looping", ShortTooltip = "A GameplayCueNotify that has a duration that is driven by the game."))
+class AAkGameplayCueNotify_Looping : public AGameplayCueNotify_Actor
+{
+	GENERATED_BODY()
+
+public:
+	UE_API AAkGameplayCueNotify_Looping(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	/** ~ Begin AGameplayCueNotify_Actor Interface */
+	UE_API virtual bool Recycle() override;
+
+	UE_API virtual bool OnActive_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) override;
+	UE_API virtual bool WhileActive_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) override;
+	UE_API virtual bool OnExecute_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) override;
+	UE_API virtual bool OnRemove_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) override;
+
+	UE_API virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+#if WITH_EDITOR
+	UE_API virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif
+	/** ~ End AGameplayCueNotify_Actor Interface */
+
+	UE_API virtual void RemoveLoopingEffects();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	UE_API void OnApplication(AActor* Target, const FGameplayCueParameters& Parameters, const FAkGameplayCueNotify_SpawnResult& SpawnResults);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	UE_API void OnLoopingStart(AActor* Target, const FGameplayCueParameters& Parameters, const FAkGameplayCueNotify_SpawnResult& SpawnResults);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	UE_API void OnRecurring(AActor* Target, const FGameplayCueParameters& Parameters, const FAkGameplayCueNotify_SpawnResult& SpawnResults);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	UE_API void OnRemoval(AActor* Target, const FGameplayCueParameters& Parameters, const FAkGameplayCueNotify_SpawnResult& SpawnResults);
+
+protected:
+	/** Default condition to check before spawning anything.  Applies for all spawns unless overridden. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Defaults")
+	FGameplayCueNotify_SpawnCondition DefaultSpawnCondition;
+
+	/** Default placement rules.  Applies for all spawns unless overridden. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Defaults")
+	FGameplayCueNotify_PlacementInfo DefaultPlacementInfo;
+
+	/** List of effects to spawn on application.  These should not be looping effects! */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Application Effects (On Active)")
+	FAkGameplayCueNotify_BurstEffects ApplicationEffects;
+
+	/** Results of spawned application effects. */
+	UPROPERTY(BlueprintReadOnly, Category = "GCN Application Effects (On Active)")
+	FAkGameplayCueNotify_SpawnResult ApplicationSpawnResults;
+
+	/** List of effects to spawn on loop start. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Looping Effects (While Active)")
+	FAkGameplayCueNotify_LoopingEffects LoopingEffects;
+
+	/** Results of spawned looping effects. */
+	UPROPERTY(BlueprintReadOnly, Category = "GCN Looping Effects (While Active)")
+	FAkGameplayCueNotify_SpawnResult LoopingSpawnResults;
+
+	/** List of effects to spawn for a recurring gameplay effect (e.g. each time a DOT ticks).  These should not be looping effects! */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Recurring Effects (On Execute)")
+	FAkGameplayCueNotify_BurstEffects RecurringEffects;
+
+	/** Results of spawned recurring effects. */
+	UPROPERTY(BlueprintReadOnly, Category = "GCN Recurring Effects (On Execute)")
+	FAkGameplayCueNotify_SpawnResult RecurringSpawnResults;
+
+	/** List of effects to spawn on removal.  These should not be looping effects! */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Removal Effects (On Remove)")
+	FAkGameplayCueNotify_BurstEffects RemovalEffects;
+
+	/** Results of spawned removal effects. */
+	UPROPERTY(BlueprintReadOnly, Category = "GCN Removal Effects (On Remove)")
+	FAkGameplayCueNotify_SpawnResult RemovalSpawnResults;
+
+	bool bLoopingEffectsRemoved;
+};
+
+#undef UE_API

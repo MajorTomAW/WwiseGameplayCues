a0ae8d46b2c631d468073299d230ef0e7f7ef262
MajorTomAW <tom.wrnr.private@gmail.com>
2026-02-23 14:24:49 +0100

    added AkGameplayCueNotify_BurstLatent class

---
+++ Source/WwiseGameplayCues/Private/AkGameplayCueNotify_BurstLatent.cpp
@@ -0,0 +1,71 @@
+﻿// Author: Tom Werner (MajorT), 2026 February
+
+
+#include "AkGameplayCueNotify_BurstLatent.h"
+
+#include "AkComponent.h"
+
+#if WITH_EDITOR
+#include "Misc/DataValidation.h"
+#endif
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AkGameplayCueNotify_BurstLatent)
+
+const float DefaultBurstLatentLifetime = 5.0f;
+
+AAkGameplayCueNotify_BurstLatent::AAkGameplayCueNotify_BurstLatent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>("RootComponent");
+
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	bAutoDestroyOnRemove = true;
+	NumPreallocatedInstances = 3;
+
+	Recycle();
+}
+
+bool AAkGameplayCueNotify_BurstLatent::Recycle()
+{
+	Super::Recycle();
+	BurstSpawnResults.Reset();
+	return true;
+}
+
+bool AAkGameplayCueNotify_BurstLatent::OnExecute_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters)
+{
+	UWorld* World = GetWorld();
+
+	FGameplayCueNotify_SpawnContext SpawnContext(World, MyTarget, Parameters);
+	SpawnContext.SetDefaultSpawnCondition(&DefaultSpawnCondition);
+	SpawnContext.SetDefaultPlacementInfo(&DefaultPlacementInfo);
+
+	if (DefaultSpawnCondition.ShouldSpawn(SpawnContext))
+	{
+		BurstEffects.ExecuteEffects(SpawnContext, BurstSpawnResults);
+		OnBurst(MyTarget, Parameters, BurstSpawnResults);
+	}
+
+	// Handle GC removal by default. This is a simple default to handle all cases we can currently think of.
+	// If we didn't do this, we'd be relying on every BurstLatent GC manually setting up its removal within BP graphs,
+	// or some inference based on parameters.
+	if (World)
+	{
+		const float Lifetime = FMath::Max<float>(AutoDestroyDelay, DefaultBurstLatentLifetime);
+		World->GetTimerManager().SetTimer(FinishTimerHandle, this, &AGameplayCueNotify_Actor::GameplayCueFinishedCallback, Lifetime);
+	}
+
+	return false;
+}
+
+#if WITH_EDITOR
+EDataValidationResult AAkGameplayCueNotify_BurstLatent::IsDataValid(class FDataValidationContext& Context) const
+{
+	BurstEffects.ValidateAssociatedAssets(this, TEXT("BurstEffects"), Context);
+
+	return ((Context.GetNumErrors() > 0) ? EDataValidationResult::Invalid : EDataValidationResult::Valid);
+}
+#endif
---
+++ Source/WwiseGameplayCues/Public/AkGameplayCueNotify_BurstLatent.h
@@ -0,0 +1,63 @@
+﻿// Author: Tom Werner (MajorT), 2026 February
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AkGameplayCueTypes.h"
+#include "GameplayCueNotifyTypes.h"
+#include "GameplayCueNotify_Actor.h"
+
+#include "AkGameplayCueNotify_BurstLatent.generated.h"
+
+#define UE_API WWISEGAMEPLAYCUES_API
+
+struct FAkGameplayCueNotify_SpawnResult;
+
+/**
+ * AAkGameplayCueNotify_BurstLatent
+ *
+ *	This is an instanced gameplay cue notify for effects that are one-offs.
+ *	Since it is instanced, it can do latent things like timelines or delays.
+ *
+ *	Supporting Ak (Wwise) audio events.
+ */
+UCLASS(Blueprintable, NotPlaceable, Category="GameplayCueNotify", MinimalAPI, meta=(ShowWorldContextPin, DisplayName="Ak GCN Burst Latent", ShortTooltip = "A one-off GameplayCueNotify that can use latent actions such as timelines."))
+class AAkGameplayCueNotify_BurstLatent : public AGameplayCueNotify_Actor
+{
+	GENERATED_BODY()
+
+public:
+	UE_API AAkGameplayCueNotify_BurstLatent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	//~ Begin AGameplayCueNotify_Actor Interface
+	UE_API virtual bool Recycle() override;
+	UE_API virtual bool OnExecute_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) override;
+
+#if WITH_EDITOR
+	UE_API virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif
+	//~ End AGameplayCueNotify_Actor Interface
+
+	UFUNCTION(BlueprintImplementableEvent)
+	UE_API void OnBurst(AActor* Target, const FGameplayCueParameters& Parameters, const FAkGameplayCueNotify_SpawnResult& SpawnResults);
+
+protected:
+	/** Default condition to check before spawning anything.  Applies for all spawns unless overridden. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Defaults")
+	FGameplayCueNotify_SpawnCondition DefaultSpawnCondition;
+
+	/** Default placement rules.  Applies for all spawns unless overridden. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Defaults")
+	FGameplayCueNotify_PlacementInfo DefaultPlacementInfo;
+
+	/** List of effects to spawn on burst. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GCN Effects")
+	FAkGameplayCueNotify_BurstEffects BurstEffects;
+
+	/** Results of spawned burst effects. */
+	UPROPERTY(BlueprintReadOnly, Category = "GCN Effects")
+	FAkGameplayCueNotify_SpawnResult BurstSpawnResults;
+};
+
+#undef UE_API
